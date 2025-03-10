740fc3f7c170ab3fc52dd6e63733a8704c16c796
lbruneaugach <lbruneaugach@pce-test>
2025-03-10 10:13:21 +0100

    add table country

--- html/data/class_country.js
+++ html/data/class_country.js
@@ -1,1 +1,38 @@
-console.log('test')
+class Country {
+    constructor (codeAlpha3, nomFrancais, capitale, continent, population, paysVoisins){
+        this._codeAlpha3 = codeAlpha3
+        this._nomFrancais = nomFrancais
+        this._capitale = capitale
+        this._continent = continent
+        this._population = population
+        this._paysVoisin = paysVoisins
+    }
+
+    get codeAlpha3 () {
+        return this._codeAlpha3
+    }
+
+    get nomFrancais () {
+        return this._nomFrancais
+    }
+
+    get capitale () {
+        return this._capitale
+    }
+
+    get continent () {
+        return this._continent
+    }
+
+    get population () {
+        return this._population
+    }
+    
+    get paysVoisins () {
+        return this.paysVoisins
+    }
+
+    toString() {
+        return this._codeAlpha3 +', '+ this._nomFrancais +', '+ this._capitale +', '+ this._continent +', '+ this._population + 'hab, (' + this._paysVoisin + ')'
+    }
+}
