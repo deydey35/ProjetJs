9f99fcd97164be94b6fa8792dfb253ed887b4319
LBG56000 <lbgzorro2005@gmail.com>
2025-03-18 19:25:38 +0100

    moreNeighbors

--- html/test/test.js
+++ html/test/test.js
@@ -7,9 +7,7 @@ const all_countries = Country.all_countries
  * Q1 - outsideTheContinent() : 
  * Tableau JS des pays (objets Country) dont au moins 
  * un pays frontalier n’est pas dans le même continent.
- */
-
-/**
+ * 
  * Permet d'avoir les pays voisins qui ne sont pas dans le même continent que leur pays
  * @returns bordersOutsideTheContinent
  */
@@ -53,9 +51,43 @@ console.table(outsideTheContinent())
  * Q2 - moreNeighbors() :
  * moreNeighbors() : Tableau des pays ayant le plus grand nombre de voisins.
  * Affichez aussi les voisins
+ * 
+ * Permet d'afficher les pays avec le plus de voisins ainsi que les voisins associés
  */
-
-
 function moreNeighbors() {
-    
+
+    // Tableau contenant les futurs pays avec le plus de voisins
+    let moreNeighbors = []
+
+    // Variable changeante permettant de stocker le longueurs du tableau voisins
+    let maxBorders = 0
+
+    // Parcourts du tableau contenant les pays
+    Object.keys(all_countries).forEach(countryKey => {
+        
+        // Initailisation d'un pays
+        const country = all_countries[countryKey]
+
+        // Initialisation des voisins de pays
+        const borders = country["borders"]
+        
+        if (borders !== undefined) {
+            // Récupération de la longueur du tableau des voisins
+            let numberOfBorders = borders.length
+            if (numberOfBorders >= maxBorders) {
+                maxBorders = numberOfBorders
+                // Ajout dans le tableau du pays avec le plus de voisins
+                moreNeighbors.push(country)
+            }
+        }
+    })
+
+    // Affichage du pays + des ses voisins
+    console.log('Affichage des pays ayant le plus de voisin')
+    moreNeighbors.forEach(country => {
+        console.log()
+        console.log(`Les voisins de ${country["frenchName"]} sont ${country.traductionOfBorders}`)
+    })
 }
+
+moreNeighbors()
